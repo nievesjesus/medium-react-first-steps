e92c09684a1ce86c3dae4caf8d74dbb19afbacc3
JesusN <yo@nievesjesus.com>
2022-01-26 23:51:06 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+    return (
+        <>
+        <h1>Hi, Welcome 🏠</h1>
+        <p>This is your first React Component, enojoy it.</p>
+        </>
+    )
+}
+
+export default Home;
